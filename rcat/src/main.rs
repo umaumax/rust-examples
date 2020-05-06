e8a31bd27632499228ef48a181fc2cc4d81dfdbc
umaumax <umauma7083@gmail.com>
2020-05-06 21:50:54 +0900

    Refactor

--- rcat/src/main.rs
+++ rcat/src/main.rs
@@ -62,10 +62,36 @@ impl FromStr for ColorWhen {
         }
     }
 }
+impl ColorWhen {
+    fn str_to_color_flag(&self, isatty: bool) -> bool {
+        match self {
+            ColorWhen::Always => true,
+            ColorWhen::Never => false,
+            ColorWhen::Auto => isatty,
+        }
+    }
+}
+
+pub fn get_buf_reader(file: &str) -> BufReader<Box<dyn std::io::Read>> {
+    let read: Box<dyn std::io::Read> = match file {
+        "-" => Box::new(io::stdin()),
+        _ => Box::new(File::open(file).expect(&(format!("Error opening {} file", file)))),
+    };
+    BufReader::new(read)
+}
+
+pub fn get_buf_reader_safe(
+    file: &str,
+) -> Result<BufReader<Box<dyn std::io::Read>>, std::io::Error> {
+    let reader: Box<dyn std::io::Read> = match file {
+        "-" => Box::new(io::stdin()),
+        _ => Box::new(File::open(file)?),
+    };
+    Ok(BufReader::new(reader))
+}
 
 fn main() {
     let matches = build_app().get_matches();
-    let color_when = ColorWhen::from_str(matches.value_of("color").unwrap());
     let base_line = matches
         .value_of("line")
         .unwrap_or("0")
@@ -76,68 +102,53 @@ fn main() {
         .unwrap_or("3")
         .parse::<i32>()
         .expect("parse context option");
+    let color_when = ColorWhen::from_str(matches.value_of("color").unwrap_or("auto"))
+        .expect("parse color option");
 
     let isatty: bool = atty::is(atty::Stream::Stdout);
-    let color_flag: bool = match color_when {
-        Ok(ColorWhen::Always) => true,
-        Ok(ColorWhen::Never) => false,
-        Ok(ColorWhen::Auto) => isatty,
-        Err(e) => {
-            eprintln!("{}", e);
-            std::process::exit(1);
-        }
-    };
+    let color_flag: bool = color_when.str_to_color_flag(isatty);
 
-    let files: Vec<_> = matches.values_of("files").unwrap().collect();
-
+    let mut files: Vec<_> = matches.values_of("files").unwrap().collect();
+    // NOTE: default input is stdin
     if files.len() == 0 {
-        let stdin: Stdin = std::io::stdin();
-        let mut buf_file = BufReader::new(stdin);
-    // TODO
-    // let _ = write_lines(&mut buf_file, f).expect("write all failed");
-    } else {
-        files
-            .iter()
-            .for_each(|filename| match File::open(filename) {
-                Ok(file) => {
-                    let mut buf_file = BufReader::new(file);
-                    // TODO
-                    // let _ = write_lines(&mut buf_file);
-                    // TODO: このクロージャを変数に代入するようにしたいが，エラーがでてしまう
-                    let _ = write_lines(&mut buf_file, |nr, s| {
-                        if base_line == nr {
-                            // NOTE: green
-                            if color_flag {
-                                print!("\x1b[32m{:>6}  {}\x1b[m", nr, s);
-                            } else {
-                                print!("{:>6}  {}", nr, s);
-                            }
-                        } else if base_line <= 0
-                            || base_line - line_context <= nr && nr <= base_line + line_context
-                        {
-                            print!("{:>6}  {}", nr, s);
-                        }
-                        if base_line > 0 && nr == base_line + line_context {
-                            return false;
-                        }
-                        true
-                    });
-                }
-                Err(e) => eprintln!(
+        files.push("-");
+    }
+    files.iter().for_each(|filename| {
+        let mut reader = match get_buf_reader_safe(filename) {
+            Ok(reader) => reader,
+            Err(e) => {
+                eprintln!(
                     "{}: '{}' at {}",
                     e,
                     filename,
                     env::current_dir().unwrap().to_string_lossy()
-                ),
-            })
-    }
-}
-
-fn write_all(istream: &mut dyn BufRead) -> Result<(), io::Error> {
-    let mut s = String::new();
-    istream.read_to_string(&mut s)?;
-    println!("{}", s);
-    Ok(())
+                );
+                return;
+            }
+        };
+        // TODO: ディレクトリを指定したときの挙動確認...?
+        // TODO: このクロージャを変数に代入するようにしたいが，エラーがでてしまう
+        let _ = write_lines(&mut reader, |nr, s| {
+            // TODO: 6を定数に
+            // このif文が煩雑
+            if base_line == nr {
+                // NOTE: green
+                if color_flag {
+                    print!("\x1b[32m{:>6}  {}\x1b[m", nr, s);
+                } else {
+                    print!("{:>6}  {}", nr, s);
+                }
+            } else if base_line <= 0
+                || base_line - line_context <= nr && nr <= base_line + line_context
+            {
+                print!("{:>6}  {}", nr, s);
+            }
+            if base_line > 0 && nr == base_line + line_context {
+                return false;
+            }
+            true
+        });
+    });
 }
 
 fn write_lines<R, F>(r: &mut R, f: F) -> Result<(), io::Error>
@@ -149,7 +160,7 @@ where
     let mut nr = 1;
     loop {
         match r.read_line(&mut s) {
-            Ok(0) => break, // EOR
+            Ok(0) => break, // EOF
             Ok(_) => {
                 let ret = f(nr, &s);
                 s.clear();
